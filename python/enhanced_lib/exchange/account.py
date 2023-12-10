81d462bd78fe222dfd61677aca4d149586ae40e4
Abiola <gbozee@gmail.com>
2023-12-10 00:11:20 +0000

    removed clients dependency

--- python/enhanced_lib/exchange/account.py
+++ python/enhanced_lib/exchange/account.py
@@ -1,43 +1,22 @@
-from .client import (
-    TradeClient,
-    loop_helper,
-    Constants,
-    GetExchangeInfoType,
-    CreateSpotProfileType,
-    CandleStickParamType,
-)
-from typing import Union, Optional
+from typing import Optional, Any
 
 
 from .types import (
     ProfileDict,
 )
 
+
 class Account:
-    def __init__(self, owner: str, config_owner: str = None, base_url=""):
+    def __init__(self, owner: str, config_owner: str = None, client: Any = None):
         self.owner = owner
         self.config_owner = config_owner or owner
         self.general_config = {
             "position_information": {},
             "config": {},
         }
-        self.base_url = base_url
-
-    async def client_helper(
-        self,
-        method: str,
-        params: Union[GetExchangeInfoType, CreateSpotProfileType, CandleStickParamType],
-    ):
-        client = TradeClient(self.base_url)
-        return await loop_helper(lambda: getattr(client, method)(params))
 
     async def fetch_config(self, symbol: str):
-        result = await self.client_helper(
-            Constants.GET_SPOT_VALUE,
-            {"owner": self.config_owner, "symbol": symbol, "spot_symbol": symbol},
-        )
-        self.general_config["config"][symbol.upper()] = result
-        return result
+        raise NotImplementedError
 
     async def update_config_fields(self, symbol: str, fields: ProfileDict):
         config = self.general_config["config"].get(symbol.upper()) or {}
@@ -47,40 +26,10 @@ class Account:
     async def update_config_resistance(
         self, symbol: str, resistance: Optional[float] = None
     ):
-        _resistance = resistance
-        if not _resistance:
-            result = await self.client_helper(
-                Constants.GET_CANDLESTICKS,
-                {
-                    "symbol": symbol,
-                    "interval": "monthly",
-                    "count": 1,
-                },
-            )
-            if result:
-                _resistance = max([x["high"] for x in result])
-        if _resistance:
-            await self.update_config_fields(symbol, {"resistance": _resistance})
+        raise NotImplementedError
 
     async def fetch_latest(self, symbol: str, save=True):
-        result = await self.client_helper(
-            Constants.EXCHANGE_INFO,
-            {"owner": self.owner, "symbol": symbol, "future_only": True, "save": save},
-        )
-        print("result", result)
-        self.general_config["position_information"][symbol.upper()] = {
-            "payload": result
-        }
-        return result
+        raise NotImplementedError
 
     async def save_config(self, symbol: str, profile: ProfileDict):
-        result = await self.client_helper(
-            Constants.SAVE_SPOT_PROFILE,
-            {
-                "owner": self.config_owner,
-                "profile": profile,
-                "symbol": symbol,
-                "spot_symbol": symbol,
-            },
-        )
-        self.general_config["config"][symbol.upper()] = result
+        raise NotImplementedError
